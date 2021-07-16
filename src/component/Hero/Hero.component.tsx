560d351e128408736283cec65d4d15c2ba320467
Muhammad Tegar Akmal <eategar@gmail.com>
2021-07-16 14:32:27 +0700

    responsive hero

--- src/component/Hero/Hero.component.tsx
+++ src/component/Hero/Hero.component.tsx
@@ -3,8 +3,8 @@ import "./Hero.scss";
 function Hero() {
   return (
     <div className="hero">
-      <div className="flex flex-col items-center justify-center h-full textContainer">
-        <div className="flex flex-col w-1/2 -mt-20 text-center text">
+      <div className="flex flex-col justify-center h-full lg:items-center textContainer">
+        <div className="flex flex-col -mt-56 md:-mt-20 lg:-mt-20 lg:w-1/2 lg:text-center text">
           <h1 className="text__main">IMAGINE A PLACE...</h1>
           <div className=" text__sub">
             ...where you can belong to a school club, a gaming group, or a
@@ -13,18 +13,18 @@ function Hero() {
             and hang out more often.
           </div>
         </div>
-        <div className="flex mt-10">
-          <button className="py-4 text-xl font-bold text-gray-800 bg-white rounded-full px-7">
-            Open Discord in your browser
+        <div className="flex flex-col w-2/3 mt-10 lg:justify-center lg:flex-col lg:w-1/3 xl:flex-row xl:w-full">
+          <button className="py-4 text-lg font-bold text-gray-800 bg-white rounded-full lg:text-xl xl:px-7 ">
+            Download For Windows
           </button>
-          <button className="py-4 ml-5 text-xl font-bold text-white bg-gray-800 rounded-full px-7">
+          <button className="py-4 mt-2 text-lg font-bold text-white bg-gray-800 rounded-full lg:mt-5 xl:mt-0 xl:ml-5 lg:text-xl xl:px-7">
             Open Discord in your browser
           </button>
         </div>
       </div>
       <div className="imageCointaner">
         <img src="/assets/images/1.svg" alt="" className="image1" />
-        <img src="/assets/images/2.svg" alt="" className="image2" />
+        <img src="/assets/images/2.svg" alt="" className=" image2" />
         <img src="/assets/images/3.svg" alt="" className="image3" />
       </div>
     </div>
