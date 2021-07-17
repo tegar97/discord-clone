e161f7230cc6c2ac3ea5d68815e461f939374c4e
Muhammad Tegar Akmal <eategar@gmail.com>
2021-07-17 18:22:41 +0700

    sidebar logo group finish

--- src/component/sidebar-channel/sidebar-channel.tsx
+++ src/component/sidebar-channel/sidebar-channel.tsx
@@ -1,44 +1,31 @@
 import React from "react";
+import ChannelsLogo from "../channels-logo/channels-logo";
 import "./sidebar-channel.scss";
 function SideBarChannel() {
   return (
-    <div className="sidebar">
-      <div className="sidebar-box">
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
-        <p className="text-white">tess</p>
+    <div className="py-5 sidebar">
+      <div className="flex flex-col items-center sidebar-box">
+        <ChannelsLogo image="/assets/images/logo_dummy.jpg" meChannel />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
+        <ChannelsLogo image="https://cdn.discordapp.com/icons/769745495206920212/27dbf63130691e8e948035c5ee459c26.png?size=256" />
       </div>
     </div>
   );
